8ab1dfee4991bb57bb177bb6a3b5d57d5c256d90
EAgha <eaghayevv@gmail.com>
2021-07-28 18:18:18 +0400

    project added

---
+++ Pustok/Pustok/Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Pustok.DAL;
+using Pustok.Models;
+using Pustok.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(AppDbContext context,UserManager<AppUser> userManager,SignInManager<AppUser> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(UserRegisterViewModel registerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if( _userManager.Users.Any(x=>x.NormalizedUserName == registerVM.UserName.ToUpper()))
+            {
+                ModelState.AddModelError("UserName", "UserName already taken!");
+                return View();
+            }
+
+            if(_userManager.Users.Any(x=>x.NormalizedEmail == registerVM.Email.ToUpper()))
+            {
+                ModelState.AddModelError("Email", "Email already taken!");
+                return View();
+            }
+
+            AppUser appUser = new AppUser
+            {
+                UserName = registerVM.UserName,
+                Email = registerVM.Email,
+                FullName = registerVM.FullName
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(appUser, registerVM.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+                return View();
+            }
+
+            await _signInManager.SignInAsync(appUser, true);
+
+
+            return RedirectToAction("index","home");
+        }
+
+        //public IActionResult GetInfo()
+        //{
+        //    var user = _userManager.Users.FirstOrDefault(x => x.UserName == User.Identity.Name);
+
+        //    return Json(user);
+        //}
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("index", "home");
+        }
+    }
+}
---
+++ Pustok/Pustok/ViewModels/UserRegisterViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.ViewModels
+{
+    public class UserRegisterViewModel
+    {
+        [StringLength(maximumLength: 50)]
+        public string FullName { get; set; }
+        [Required]
+        [StringLength(maximumLength:25)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 100)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [StringLength(maximumLength: 20)]
+        [DataType(DataType.Password)]
+
+        [Required]
+        public string Password { get; set; }
+        [StringLength(maximumLength: 20)]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassowrd { get; set; }
+    }
+}
